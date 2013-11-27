59f4dcc9aebf405afe0f3286f8356333e1ec8cd5
Edgar Thier <edgarthier@gmail.com>
2013-11-27 11:54:06 +0100

    Inserted Apache License header into necessary files

--- gige/camera-ip-conf/main.cpp
+++ gige/camera-ip-conf/main.cpp
@@ -1,10 +1,18 @@
-///
-/// @file main.cpp
-///
-/// @Copyright (C) 2013 The Imaging Source GmbH; Edgar Thier <edgarthier@gmail.com>
-///
-/// main file for the commandline client that allows IP configuration of GigE cameras
-///
+/*
+ * Copyright 2013 The Imaging Source Europe GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #include "ConsoleManager.h"
 
