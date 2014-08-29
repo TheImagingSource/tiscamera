237af835c6bcfb401a9483177a76dc57df97db29
Edgar Thier <edgar.thier@theimagingsource.com>
2014-08-29 12:00:17 +0200

    Added documentation

--- src/DeviceInterface.h
+++ src/DeviceInterface.h
@@ -32,12 +32,19 @@ public:
 
     virtual CaptureDevice getDeviceDescription () const = 0;
 
+    /**
+     * @brief Returns all device properties
+     */
     virtual std::vector<std::shared_ptr<Property>> getProperties () = 0;
 
     virtual bool setProperty (const Property&) = 0;
 
     virtual bool getProperty (Property&) = 0;
 
+    /**
+     * @brief Set Format in he actual device
+     * @return True on success; False on error or invalid format
+     */
     virtual bool setVideoFormat (const VideoFormat&) = 0;
      
     virtual VideoFormat getActiveVideoFormat () const = 0;
@@ -48,6 +55,10 @@ public:
     virtual bool setSink (std::shared_ptr<SinkInterface>) = 0;
 
     virtual bool initialize_buffers (std::vector<std::shared_ptr<MemoryBuffer>>) = 0;
+
+    /**
+     * @brief Delete all internal references to used memorybuffers
+     */
     virtual bool release_buffers () = 0;
 
     virtual bool start_stream () = 0;
@@ -56,6 +67,12 @@ public:
 
 }; /* class Camera_Interface*/
 
+
+/**
+ * @brief
+ * @param device - device description for which an interface shall be created
+ * @return shared_ptr containing the device; nullptr on error
+ */
 std::shared_ptr<DeviceInterface> openDeviceInterface (const CaptureDevice& device);
 
 } /* namespace tis_imaging */
