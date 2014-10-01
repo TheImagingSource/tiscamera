22b750213346e8a14e38ae1e7f3eb1e53f1ac013
Edgar Thier <edgar.thier@theimagingsource.com>
2014-10-01 14:54:37 +0200

    Added documentation

--- src/PropertyImpl.h
+++ src/PropertyImpl.h
@@ -10,6 +10,9 @@ namespace tis_imaging
 
 class Property;
 
+/**
+ * Interface used by property implementations
+ */
 class PropertyImpl
 {
     
@@ -19,8 +22,16 @@ public:
 
     virtual bool isAvailable (const Property&) = 0;
 
+    /**
+     * Set internal properties to the values described in parameter
+     * @return true is given property values where successfully applied; else false
+     */
     virtual bool setProperty (const Property&) = 0;
-    
+
+    /**
+     * Fill given property with the currently used values
+     * @return true if property was successfully filled; else false
+     */
     virtual bool getProperty (Property&) = 0;
 };
 
