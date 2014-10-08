a3eba24215eb1d6e56bd6685a32d6df76e1f607c
Edgar Thier <edgar.thier@theimagingsource.com>
2014-10-08 10:30:56 +0200

    Added setStatus / getStatus to SinkInterface

--- src/SinkInterface.h
+++ src/SinkInterface.h
@@ -16,6 +16,10 @@ public:
 
     virtual ~SinkInterface () {};
 
+    virtual bool setStatus (const PIPELINE_STATUS&) = 0;
+
+    virtual PIPELINE_STATUS getStatus () const = 0;
+
     virtual void pushImage (std::shared_ptr<MemoryBuffer>) = 0;
     
 };
