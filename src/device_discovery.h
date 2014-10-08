c57eb1243d2f1c0028679d778a77b3b39cc47480
Edgar Thier <edgar.thier@theimagingsource.com>
2014-10-08 14:20:47 +0200

    Added documentation

--- src/device_discovery.h
+++ src/device_discovery.h
@@ -15,7 +15,7 @@ extern "C"
 
     /**
      * @name tis_get_camera_count
-     * @return 
+     * @return number of available devices
      */
     int tis_get_camera_count ();
     
@@ -23,18 +23,39 @@ extern "C"
      * @name
      * @param ptr        - pointer to the array that shall be filled
      * @param array_size - size of array that ptr points to
-     * @return 
+     * @return number of devices copied to ptr; -1 on error
      */
     int tis_get_camera_list (struct tis_device_info* ptr, unsigned int array_size);
 
+
+    /**
+     * @name tis_get_camera_count
+     * @return number of available usb devices
+     */
     int tis_get_usb_camera_count ();
     
+    /**
+     * @name
+     * @param ptr        - pointer to the array that shall be filled
+     * @param array_size - size of array that ptr points to
+     * @return number of devices copied to ptr; -1 on error
+     */
     int tis_get_usb_camera_list (struct tis_device_info* ptr, unsigned int array_size);
 
 #if HAVE_ARAVIS
-    
+
+    /**
+     * @name tis_get_camera_count
+     * @return number of available gige devices
+     */
     int tis_get_gige_camera_count ();
-    
+
+    /**
+     * @name
+     * @param ptr        - pointer to the array that shall be filled
+     * @param array_size - size of array that ptr points to
+     * @return number of devices copied to ptr; -1 on error
+     */
     int tis_get_gige_camera_list (struct tis_device_info* ptr, unsigned int array_size);
 
 #endif
