10b31ff576e80e102e1e74cbbab57c5641e48850
Edgar Thier <edgar.thier@theimagingsource.com>
2014-12-05 09:31:38 +0100

    Switched v4l2 property generation to create_empty_property

--- src/v4l2_utils.cpp
+++ src/v4l2_utils.cpp
@@ -277,10 +277,8 @@ std::shared_ptr<Property> tcam::createProperty (int fd,
     }
     else
     {
-        cp.id = ctrl_m.id;
         type_to_use = ctrl_m.type_to_use;
-        memcpy(cp.name, ctrl_m.name.c_str(), ctrl_m.name.size());
-        cp.type = ctrl_m.type_to_use;
+        cp = create_empty_property(ctrl_m.id);
     }
 
     uint32_t flags = convertV4L2flags(queryctrl->flags);
