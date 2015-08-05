12ec32d85751ff5f1b7c4f341537707bdf295845
Edgar Thier <edgar.thier@theimagingsource.com>
2015-08-05 13:19:21 +0200

    Added function tcam_allocate_image_buffer and tcam_free_image_buffer
    Convenience functions for buffer allocation

--- src/public_utils.cpp
+++ src/public_utils.cpp
@@ -15,3 +15,42 @@ uint32_t tcam_description_to_fourcc (const char* description)
 {
     return description2fourcc(description);
 }
+
+
+struct tcam_image_buffer* tcam_allocate_image_buffers (const struct tcam_video_format* format,
+                                                       size_t n_buffers)
+{
+    struct tcam_image_buffer* ptr = nullptr;
+
+    if (format != nullptr && n_buffers > 0)
+    {
+        // allocate buffer array
+        ptr = (struct tcam_image_buffer*)malloc(sizeof(struct tcam_image_buffer) * n_buffers);
+
+        unsigned int length = 0;
+        // allocate the actual image data fields
+        for (unsigned int i = 0; i < n_buffers; ++i)
+        {
+            struct tcam_image_buffer* tmp = &ptr[i];
+
+            tmp->pData = (unsigned char*)malloc(tcam_get_required_buffer_size(format));
+
+            // fill the rest
+            tmp->length = length;
+            tmp->format = *format;
+            tmp->pitch = get_pitch_length(format->width, format->fourcc);
+        }
+
+    }
+
+    return ptr;
+}
+
+
+void tcam_free_image_buffers (struct tcam_image_buffer* ptr, size_t n_buffer)
+{
+    if (ptr == nullptr || n_buffer < 1)
+        return;
+
+    free (ptr);
+}
--- src/public_utils.h
+++ src/public_utils.h
@@ -4,6 +4,10 @@
 
 #include <stdint.h>
 
+#include <stddef.h> /* size_t */
+
+#include "base_types.h"
+
 #if __cplusplus
 extern "C"
 {
@@ -13,6 +17,21 @@ const char* tcam_fourcc_to_description (uint32_t fourcc);
 
 
 uint32_t tcam_description_to_fourcc (const char* description);
+
+
+/**
+ * @param format - format description that shall be used
+ * @param n_buffers - number of buffers that shall be allocated
+ * @return pointer to the first image buffer
+ */
+struct tcam_image_buffer* tcam_allocate_image_buffers (const struct tcam_video_format* format,
+                                                       size_t n_buffers);
+
+/**
+ * @param ptr - pointer to the first buffer that shall be freed
+ * @param n_buffers - number of buffers that shall be freed
+ */
+void tcam_free_image_buffers (struct tcam_image_buffer* ptr, size_t n_buffer);
 
 
 #if __cplusplus
