5c5687a96834d4ca8de78fa55a0dd4cba1c1b5f9
Edgar Thier <edgar.thier@theimagingsource.com>
2015-09-21 10:18:01 +0200

    Add dependency list to pkg-config

--- data/tcam.pc.cmake
+++ data/tcam.pc.cmake
@@ -6,6 +6,6 @@ includedir=${prefix}/include
 Name: Tcam
 Description: Camera control and image acquisition library
 Version: @TCAM_VERSION@
-Requires: glib-2.0 gobject-2.0 gio-2.0 libxml-2.0 gthread-2.0
+Requires: @tcam_pkgconfig_dependencies@
 Libs: -L${libdir} -ltcam
 Cflags: -I${includedir}/
