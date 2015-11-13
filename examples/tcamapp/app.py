402cb6b2db0c38a2f59c9c32fa6390d70e10605a
Arne Caspari <arne@unicap-imaging.org>
2015-11-13 14:37:44 +0100

    tcamapp: Add GI version requirements

--- examples/tcamapp/app.py
+++ examples/tcamapp/app.py
@@ -3,6 +3,8 @@ import gi
 gi.require_version ("Gtk", "3.0")
 gi.require_version ("Gst", "1.0")
 gi.require_version ("Tcam", "0.1")
+gi.require_version ("GdkX11", "3.0")
+gi.require_version ("GstVideo", "1.0")
 
 from gi.repository import GdkX11, Gtk, Tcam, GstVideo, Gst, GdkPixbuf
 
