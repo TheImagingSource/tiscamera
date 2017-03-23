4ffb156e5ba0eb06b7b652304afee450d29cc5fb
Edgar Thier <edgar.thier@theimagingsource.com>
2017-03-23 10:23:35 +0100

    Add support for aravis-0.6 as a dependency

--- cmake/modules/FindAravis.cmake
+++ cmake/modules/FindAravis.cmake
@@ -11,6 +11,7 @@ include(LibFindMacros)
 
 # Use pkg-config to get hints about paths
 libfind_pkg_check_modules(aravis_PKGCONF aravis-0.4)
+libfind_pkg_check_modules(aravis0_6_PKGCONF aravis-0.6)
 
 # Include dir
 find_path(aravis_INCLUDE_DIR
@@ -18,21 +19,27 @@ find_path(aravis_INCLUDE_DIR
 	arv.h
 	PATHS
 	${aravis_PKGCONF_INCLUDE_DIRS}
+	${aravis0_6_PKGCONF_INCLUDE_DIRS}
 	/usr/local/include
 	/usr/local/include/aravis-0.4
+	/usr/local/include/aravis-0.6
 	/usr/include
 	/usr/include/aravis-0.4
+	/usr/include/aravis-0.6
 )
 
 # Finally the library itself
 find_library(aravis_LIBRARY
 	NAMES
 	libaravis-0.4
+	libaravis-0.6
 	aravis
 	aravis-0.4
+	aravis-0.6
 	libaravis
 	PATHS
 	${aravis_PKGCONF_LIBRARY_DIRS}
+	${aravis0_6_PKGCONF_LIBRARY_DIRS}
 	/usr/local/lib
 	/usr/lib
 )
