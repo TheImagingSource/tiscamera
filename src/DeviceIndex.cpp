TheImagingSource/tiscamera

commit 732a0ed092d931e574b7bfa66cd83b2ae4709291

Minimize waiting period for first valid device list

src/DeviceIndex.cpp
@@ -41,7 +41,7 @@ using namespace tcam;
 DeviceIndex::DeviceIndex ()
     : continue_thread(false),
       wait_period(2),
-      init_count(0),
+      have_list(false),
       device_list(std::vector<DeviceInfo>()),
       callbacks(std::vector<callback_data>())
 {
@@ -169,10 +169,7 @@ void DeviceIndex::update_device_list ()
 
     device_list.insert(device_list.end(), tmp_dev_list.begin(), tmp_dev_list.end());
 
-    if (init_count < INIT_MAX)
-    {
-        init_count++;
-    }
+    have_list = true;
     mtx.unlock();
 }
 
@@ -234,9 +231,12 @@ bool DeviceIndex::fill_device_info (DeviceInfo& info) const
 std::vector<DeviceInfo> DeviceIndex::get_device_list () const
 {
 
-    while (init_count < INIT_MAX)
+    // wait for work_thread to deliver first valid list
+    // since get_aravis_device_list is a blocking function
+    // our thread would retrieve an empty list without this wait loop
+    while(!have_list)
     {
-        usleep(500);
+        // wait
     }
 
     return device_list;

src/DeviceIndex.h
@@ -92,8 +92,7 @@ private:
     unsigned int wait_period;
     std::thread work_thread;
 
-    static const unsigned int INIT_MAX = 2;
-    unsigned int init_count;
+    bool have_list;
 
     std::vector<DeviceInfo> device_list;
 

commit 82f9af02393adacdc401492b7cff9fde2c027848

DeviceIndex: get_device_list: add timeout while waiting
Add a 500ms timeout while waiting for the device list. This reduces
CPU load dramatically. Furthermore on our custom i.MX6 board sometimes
the detection of the device failed without this patch. In these cases
this wait-for-device-list looped infinitely.

Fixes: 732a0ed092d9 ("Minimize waiting period for first valid device")
Signed-off-by: Richard Leitner <richard.leitner@skidata.com>

src/DeviceIndex.cpp
@@ -210,6 +210,7 @@ std::vector<DeviceInfo> DeviceIndex::get_device_list () const
     while(!have_list)
     {
         // wait
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
     }
 
     return device_list;