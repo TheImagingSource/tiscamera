59d89b143e39f58735b15570555ab08ea21c0a1a
Edgar Thier <edgar.thier@theimagingsource.com>
2017-10-11 09:57:44 +0200

    Add MemoryBuffer get_buffer_size and get_image_size

--- src/MemoryBuffer.h
+++ src/MemoryBuffer.h
@@ -56,6 +56,9 @@ public:
      */
     unsigned char* get_data ();
 
+    size_t get_buffer_size () const;
+    size_t get_image_size () const;
+
 
     struct tcam_stream_statistics get_statistics () const;
 
