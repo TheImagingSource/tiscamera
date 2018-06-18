e54e8f4c6176efee91aaa4ef52a0f67127562161
Edgar Thier <edgar.thier@theimagingsource.com>
2018-06-18 11:36:31 +0200

    Separate options into different tabs
    Saving related options can now be found in a separate tab.
    This simplifies future extension concerning other option types.

--- tools/tcam-capture/tcam_capture/OptionsDialog.py
+++ tools/tcam-capture/tcam_capture/OptionsDialog.py
@@ -13,16 +13,16 @@
 # limitations under the License.
 
 from . import Encoder
-from . import Settings
+from .Settings import Settings, FileNameSettings
 
 import logging
 
-# from PyQt5.QtGui import Q
-from PyQt5.QtWidgets import (QDialog, QGridLayout, QVBoxLayout,
+from PyQt5.QtWidgets import (QDialog, QVBoxLayout,
                              QLabel, QLineEdit, QComboBox,
                              QDialogButtonBox, QFileDialog,
                              QPushButton, QHBoxLayout, QCheckBox,
-                             QFormLayout)
+                             QFormLayout, QTabWidget, QWidget,
+                             QGroupBox, QSpinBox)
 from PyQt5.QtCore import Qt
 
 log = logging.getLogger(__file__)
@@ -32,76 +32,29 @@ class OptionsDialog(QDialog):
     def __init__(self, setting: Settings, have_dutils, parent=None):
         super(OptionsDialog, self).__init__(parent)
 
-        encoder_dict = Encoder.get_encoder_dict()
         self.settings = setting
-        self.enabled_video = False
+        self.enabled_video = True  # temporary toggle to disable video features as they do not exist
+        self.enabled_logging = True
+        self.enabled_keybindings = True
         self.enabled_dutils = have_dutils
         self.setWindowTitle("Tcam-Capture Options")
         self.layout = QVBoxLayout(self)
         self.setLayout(self.layout)
 
-        self.form_layout = QFormLayout()
-        self.form_layout.setSpacing(20)
-        self.form_layout.setVerticalSpacing(20)
-        self.layout.addItem(self.form_layout)
+        self.tabs = QTabWidget()
 
-        self.location_layout = QHBoxLayout()
-        self.location_label = QLabel("Where to save images/videos:", self)
-        self.location_edit = QLineEdit(self)
-        self.location_dialog_button = QPushButton("...", self)
-        self.location_dialog_button.clicked.connect(self.open_file_dialog)
-        self.location_layout.addWidget(self.location_edit)
-        self.location_layout.addWidget(self.location_dialog_button)
+        self.general_widget = QWidget()
+        self.keybindings_widget = QWidget()
+        self.logging_widget = QWidget()
+        self.saving_widget = QWidget()
 
-        # maintain descriptions as own labels
-        # pyqt seems to loose the descriptions somewhere
-        # when simple strings are used or the qlabel do ot have self as owner
-        self.form_layout.addRow(self.location_label,
-                                self.location_layout)
+        self._setup_general_ui()
+        self.tabs.addTab(self.general_widget, "General")
 
-        self.image_type_combobox = QComboBox(self)
-        for key, value in encoder_dict.items():
-            if value.encoder_type == Encoder.EncoderType.image:
-                self.image_type_combobox.addItem(key)
-        self.image_type_label = QLabel("Save images as:", self)
-        self.form_layout.addRow(self.image_type_label,
-                                self.image_type_combobox)
-        if self.enabled_video:
-            self.video_type_combobox = QComboBox(self)
-            for key, value in encoder_dict.items():
-                if value.encoder_type == Encoder.EncoderType.video:
-                    self.video_type_combobox.addItem(key)
-            self.video_type_label = QLabel("Save videos as:", self)
-            self.form_layout.addRow(self.video_type_label,
-                                    self.video_type_combobox)
+        self._setup_saving_ui()
+        self.tabs.addTab(self.saving_widget, "Image/Video")
 
-        self.device_dialog_checkbox = QCheckBox(self)
-        self.device_dialog_label = QLabel("Open device dialog on start:", self)
-        self.form_layout.addRow(self.device_dialog_label,
-                                self.device_dialog_checkbox)
-
-        self.reopen_device_checkbox = QCheckBox(self)
-        self.reopen_device_label = QLabel("Reopen device on start(ignores device dialog):", self)
-        self.form_layout.addRow(self.reopen_device_label,
-                                self.reopen_device_checkbox)
-
-
-        # self.set_device_properties_checkbox = QCheckBox(self)
-        # set_device_properties_label = QLabel("Set properties to cached values when known device is opened:", self)
-        # self.form_layout.addRow(set_device_properties_label,
-        #                         self.set_device_properties_checkbox)
-
-        self.use_dutils_checkbox = QCheckBox(self)
-        self.use_dutils_label = QLabel("Use tiscamera dutils, if present:", self)
-        self.form_layout.addRow(self.use_dutils_label,
-                                self.use_dutils_checkbox)
-
-        if not self.enabled_dutils:
-            self.use_dutils_label.setToolTip("Enabled when tiscamera-dutils are installed")
-            self.use_dutils_label.setEnabled(False)
-            self.use_dutils_checkbox.setToolTip("Enabled when tiscamera-dutils are installed")
-            self.use_dutils_checkbox.setEnabled(False)
-
+        self.layout.addWidget(self.tabs)
         # OK and Cancel buttons
         self.buttons = QDialogButtonBox(
             QDialogButtonBox.Reset | QDialogButtonBox.Ok | QDialogButtonBox.Cancel,
@@ -112,6 +65,208 @@ class OptionsDialog(QDialog):
         self.buttons.rejected.connect(self.reject)
         self.buttons.clicked.connect(self.clicked)
 
+    def _setup_general_ui(self):
+        """
+        Create everything related to the general tab
+        """
+
+        layout = QFormLayout()
+        layout.setSpacing(20)
+        layout.setVerticalSpacing(20)
+
+        self.device_dialog_checkbox = QCheckBox(self)
+        device_dialog_label = QLabel("Open device dialog on start:")
+        layout.addRow(device_dialog_label,
+                      self.device_dialog_checkbox)
+
+        self.reopen_device_checkbox = QCheckBox(self)
+        reopen_device_label = QLabel("Reopen device on start(ignores device dialog):", self)
+        layout.addRow(reopen_device_label,
+                      self.reopen_device_checkbox)
+
+        self.use_dutils_checkbox = QCheckBox(self)
+        self.use_dutils_label = QLabel("Use tiscamera dutils, if present:", self)
+        layout.addRow(self.use_dutils_label,
+                      self.use_dutils_checkbox)
+
+        if not self.enabled_dutils:
+            self.use_dutils_label.setToolTip("Enabled when tiscamera-dutils are installed")
+            self.use_dutils_label.setEnabled(False)
+            self.use_dutils_checkbox.setToolTip("Enabled when tiscamera-dutils are installed")
+            self.use_dutils_checkbox.setEnabled(False)
+
+        self.general_widget.setLayout(layout)
+
+    def _setup_saving_ui(self):
+        """
+        Create everything related to the image/video saving tab
+        """
+        encoder_dict = Encoder.get_encoder_dict()
+        form_layout = QFormLayout()
+
+        layout = QVBoxLayout()
+        layout.addLayout(form_layout)
+
+        location_layout = QHBoxLayout()
+        location_label = QLabel("Where to save images/videos:", self)
+        self.location_edit = QLineEdit(self)
+        location_dialog_button = QPushButton("...", self)
+        location_dialog_button.clicked.connect(self.open_file_dialog)
+        location_layout.addWidget(self.location_edit)
+        location_layout.addWidget(location_dialog_button)
+
+        # maintain descriptions as own labels
+        # pyqt seems to loose the descriptions somewhere
+        # when simple strings are used or the qlabel does not have self as owner
+        form_layout.addRow(location_label,
+                           location_layout)
+
+        self.image_type_combobox = QComboBox(self)
+        for key, value in encoder_dict.items():
+            if value.encoder_type == Encoder.MediaType.image:
+                self.image_type_combobox.addItem(key)
+        image_type_label = QLabel("Save images as:")
+        self.image_type_combobox.currentIndexChanged['QString'].connect(self.image_name_suffix_changed)
+
+        form_layout.addRow(image_type_label,
+                           self.image_type_combobox)
+        if self.enabled_video:
+            self.video_type_combobox = QComboBox(self)
+            for key, value in encoder_dict.items():
+                if value.encoder_type == Encoder.MediaType.video:
+                    self.video_type_combobox.addItem(key)
+            video_type_label = QLabel("Save videos as:", self)
+            form_layout.addRow(video_type_label,
+                               self.video_type_combobox)
+
+        image_name_groupbox = QGroupBox("Image Name")
+        groupbox_layout = QFormLayout()
+        image_name_groupbox.setLayout(groupbox_layout)
+
+        self.image_name_preview = QLabel("<USER-PREFIX>-<SERIAL>-<FORMAT>-<TIMESTAMP>-<COUNTER>.png")
+        self.image_name_preview_description = QLabel("Images will be named like:")
+        groupbox_layout.addRow(self.image_name_preview_description,
+                               self.image_name_preview)
+
+        self.image_name_prefix = QLineEdit()
+        self.image_name_prefix.textChanged.connect(self.image_name_prefix_changed)
+        self.image_name_prefix.setMaxLength(100)
+
+        self.image_name_prefix_description = QLabel("User Prefix:", self)
+        groupbox_layout.addRow(self.image_name_prefix_description,
+                               self.image_name_prefix)
+
+        self.image_name_serial = QCheckBox(self)
+        self.image_name_serial.toggled.connect(self.image_name_properties_toggled)
+        self.image_name_serial_description = QLabel("Include Serial:")
+        groupbox_layout.addRow(self.image_name_serial_description,
+                               self.image_name_serial)
+
+        self.image_name_format = QCheckBox(self)
+        self.image_name_format.toggled.connect(self.image_name_properties_toggled)
+
+        self.image_name_format_description = QLabel("Include Format:")
+        groupbox_layout.addRow(self.image_name_format_description,
+                               self.image_name_format)
+
+        self.image_name_counter = QCheckBox(self)
+        self.image_name_counter.toggled.connect(self.image_name_properties_toggled)
+        self.image_name_counter_description = QLabel("Include Counter:")
+        groupbox_layout.addRow(self.image_name_counter_description,
+                               self.image_name_counter)
+
+        self.image_name_counter_box = QSpinBox(self)
+        self.image_name_counter_box.setRange(1, 10)
+        self.image_name_counter_box.valueChanged.connect(self.image_name_counter_changed)
+        self.image_name_counter_box_description = QLabel("Counter Size:")
+        groupbox_layout.addRow(self.image_name_counter_box_description,
+                               self.image_name_counter_box)
+
+        self.image_name_counter.toggled.connect(self.toggle_counter_box_availability)
+        self.image_name_counter.toggled.connect(self.image_name_properties_toggled)
+
+        self.image_name_timestamp = QCheckBox(self)
+        self.image_name_timestamp.toggled.connect(self.image_name_properties_toggled)
+        self.image_name_timestamp_description = QLabel("Include Timestamp:")
+        groupbox_layout.addRow(self.image_name_timestamp_description,
+                               self.image_name_timestamp)
+
+        layout.addWidget(image_name_groupbox)
+
+        self.saving_widget.setLayout(layout)
+
+    def image_name_prefix_changed(self, name: str):
+        """"""
+
+        self.settings.image_name.user_prefix = self.image_name_prefix.text()
+        self.update_image_name_preview()
+
+    def image_name_suffix_changed(self, suffix: str):
+        """"""
+
+        self.update_image_name_preview()
+
+    def image_name_counter_changed(self, name: str):
+        """"""
+        self.settings.image_name.counter_size = self.image_name_counter_box.value()
+        self.update_image_name_preview()
+
+    def image_name_properties_toggled(self):
+        """"""
+
+        self.settings.image_name.include_timestamp = self.image_name_timestamp.isChecked()
+        self.settings.image_name.include_counter = self.image_name_counter.isChecked()
+        self.settings.image_name.include_format = self.image_name_format.isChecked()
+        self.settings.image_name.include_serial = self.image_name_serial.isChecked()
+
+        self.update_image_name_preview()
+
+    def update_image_name_preview(self):
+
+        preview_string = ""
+
+        if self.settings.image_name.user_prefix != "":
+
+            max_prefix_length = 15
+            prefix = (self.settings.image_name.user_prefix[:max_prefix_length] + '..') if len(self.settings.image_name.user_prefix) > max_prefix_length else self.settings.image_name.user_prefix
+
+            preview_string += prefix
+
+        if self.settings.image_name.include_serial:
+            if preview_string != "":
+                preview_string += "-"
+            preview_string += "00001234"
+
+        if self.settings.image_name.include_format:
+            if preview_string != "":
+                preview_string += "-"
+            preview_string += "gbrg_1920x1080_15_1"
+
+        if self.settings.image_name.include_timestamp:
+            if preview_string != "":
+                preview_string += "-"
+            preview_string += "19701230T125503"
+
+        if self.settings.image_name.include_counter:
+            if preview_string != "":
+                preview_string += "-"
+            preview_string += '{message:0>{fill}}'.format(message=1,
+                                                          fill=self.settings.image_name.counter_size)
+
+        if preview_string == "":
+            preview_string = "image"
+
+        preview_string += "." + self.image_type_combobox.currentText()
+
+        self.image_name_preview.setText(preview_string)
+
+    def toggle_counter_box_availability(self):
+        """"""
+        if self.image_name_counter.isChecked():
+            self.image_name_counter_box.setEnabled(True)
+        else:
+            self.image_name_counter_box.setEnabled(False)
+
     def set_settings(self, settings: Settings):
         self.location_edit.setText(settings.get_save_location())
         self.image_type_combobox.setCurrentText(settings.get_image_type())
@@ -119,8 +274,35 @@ class OptionsDialog(QDialog):
             self.video_type_combobox.setCurrentText(settings.get_video_type())
         self.device_dialog_checkbox.setChecked(settings.show_device_dialog_on_startup)
         self.reopen_device_checkbox.setChecked(settings.reopen_device_on_startup)
-        # self.set_device_properties_checkbox.setChecked(settings.set_properties_on_reopen)
         self.use_dutils_checkbox.setChecked(settings.use_dutils)
+
+        if settings.image_name.include_timestamp:
+            self.image_name_timestamp.blockSignals(True)
+            self.image_name_timestamp.toggle()
+            self.image_name_timestamp.blockSignals(False)
+        if settings.image_name.include_counter:
+            self.image_name_counter.blockSignals(True)
+            self.image_name_counter.toggle()
+            self.image_name_counter.blockSignals(False)
+
+        self.image_name_counter_box.blockSignals(True)
+        self.image_name_counter_box.setValue(settings.image_name.counter_size)
+        self.image_name_counter_box.blockSignals(False)
+        self.toggle_counter_box_availability()
+
+        if settings.image_name.include_format:
+            self.image_name_format.blockSignals(True)
+            self.image_name_format.toggle()
+            self.image_name_format.blockSignals(False)
+        if settings.image_name.include_serial:
+            self.image_name_serial.blockSignals(True)
+            self.image_name_serial.toggle()
+            self.image_name_serial.blockSignals(False)
+        self.image_name_prefix.blockSignals(True)
+        self.image_name_prefix.setText(settings.image_name.user_prefix)
+        self.image_name_prefix.blockSignals(False)
+
+        self.update_image_name_preview()
 
     def save_settings(self):
         self.settings.save_location = self.location_edit.text()
@@ -129,8 +311,16 @@ class OptionsDialog(QDialog):
             self.settings.video_type = self.video_type_combobox.currentText()
         self.settings.show_device_dialog_on_startup = self.device_dialog_checkbox.isChecked()
         self.settings.reopen_device_on_startup = self.reopen_device_checkbox.isChecked()
-        # self.settings.set_properties_on_reopen = self.set_device_properties_checkbox.isChecked()
         self.settings.use_dutils = self.use_dutils_checkbox.isChecked()
+
+        self.settings.image_name.include_timestamp = self.image_name_timestamp.isChecked()
+        self.settings.image_name.include_counter = self.image_name_counter.isChecked()
+        if self.image_name_counter.isChecked():
+            self.settings.image_name.counter_size = self.image_name_counter_box.value()
+
+        self.settings.image_name.include_format = self.image_name_format.isChecked()
+        self.settings.image_name.include_serial = self.image_name_serial.isChecked()
+        self.settings.image_name.user_prefix = self.image_name_prefix.text()
 
     def open_file_dialog(self):
         fdia = QFileDialog()
