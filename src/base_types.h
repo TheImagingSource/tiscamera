522a7f9d0d479eb3a9d9d1800565dc1cf6998e37
Edgar Thier <edgar.thier@theimagingsource.com>
2018-09-05 07:30:22 +0200

    Add is_damaged field to statistics

--- src/base_types.h
+++ src/base_types.h
@@ -195,6 +195,7 @@ struct tcam_stream_statistics
     uint64_t capture_time_ns;  /**< capture time reported by lib */
     uint64_t camera_time_ns;   /**< capture time reported by camera; empty if not supported */
     double   framerate;        /**< in contrast to selected one */
+    bool is_damaged;           /**< flag indicating if the associated buffer had lost packages or other problems */
 };
 
 /**
