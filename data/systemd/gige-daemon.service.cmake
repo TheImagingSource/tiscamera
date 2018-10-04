5e4ed541eac718392bf53ff4e8049dd2fde07615
Edgar Thier <edgar.thier@theimagingsource.com>
2018-10-04 06:26:25 +0200

    systemd: Add pidfile entry for gige-daemon
    This ensures the lock file is cleaned up when the daemon not correctly stops.

--- data/systemd/gige-daemon.service.cmake
+++ data/systemd/gige-daemon.service.cmake
@@ -4,6 +4,7 @@ Requires=network.target
 After=network.target
 
 [Service]
+PIDFile=/var/lock/gige-daemon.lock
 Type=simple
 ExecStart=@CMAKE_INSTALL_PREFIX@/bin/gige-daemon start --no-fork
 ExecStop=@CMAKE_INSTALL_PREFIX@/bin/gige-daemon stop
