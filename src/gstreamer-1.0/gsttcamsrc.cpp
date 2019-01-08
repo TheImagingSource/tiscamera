e39d0d6729825d5c6389d5cac8d97e87941b358f
Edgar Thier <edgar.thier@theimagingsource.com>
2019-01-08 08:35:50 +0100

    tcamsrc: Add serial as detail to device lost message

--- src/gstreamer-1.0/gsttcamsrc.cpp
+++ src/gstreamer-1.0/gsttcamsrc.cpp
@@ -1049,7 +1049,11 @@ static void gst_tcam_src_device_lost_callback (const struct tcam_device_info* in
 
     GST_ERROR("Received lost device notification. Stopping stream.");
 
-    GST_ELEMENT_ERROR(GST_ELEMENT(self), RESOURCE, NOT_FOUND, (("Device lost")), ((NULL)));
+    GST_ELEMENT_ERROR_WITH_DETAILS(GST_ELEMENT(self),
+                                   RESOURCE, NOT_FOUND,
+                                   ("Device lost"),
+                                   ((nullptr)),
+                                   ("serial", G_TYPE_STRING, self->device_serial.c_str(), nullptr));
 
     gst_tcam_src_stop(GST_BASE_SRC(self));
 }
