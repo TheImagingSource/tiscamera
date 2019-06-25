9c01904d12356c44683eb79266c8602b82774e2d
Edgar Thier <edgar.thier@theimagingsource.com>
2019-06-25 11:18:41 +0200

    examples: Make 04-list-formats.py support legacy ranges

--- examples/python/04-list-formats.py
+++ examples/python/04-list-formats.py
@@ -19,7 +19,6 @@
 # caps from tcamsrc and how to display print them to stdout.
 #
 
-
 import sys
 import gi
 
@@ -39,28 +38,82 @@ def print_formats(source):
 
         name = structure.get_name()
 
-        fmt = structure.get_value("format")
-        # print(type(fmt))
+        try:
+            fmt = structure.get_value("format")
 
-        if type(fmt) is str:
-            print("{} {}".format(name, fmt), end="")
-        elif type(fmt) is Gst.ValueList:
+            if type(fmt) is str:
+                print("{} {}".format(name, fmt), end="")
+            elif type(fmt) is Gst.ValueList:
+
+                print("{} {{ ".format(name), end="")
+
+                for y in range(Gst.ValueList.get_size(fmt)):
+
+                    val = Gst.ValueList.get_value(fmt, y)
+
+                    print("{} ".format(val), end="")
+                print("}", end="")
+            else:
+                print("==")
+        except TypeError:  # Gst.ValueList
+
+            # this means we have multiple formats that all
+            # have the same width/height/framerate settings
+
+            begin = structure.to_string().find("format=(string){")
+            substr = structure.to_string()[begin:]
+            values = substr[substr.find("{")+1:substr.find("}")]
 
             print("{} {{ ".format(name), end="")
 
-            for y in range(Gst.ValueList.get_size(fmt)):
+            for fmt in values.split(","):
 
-                val = Gst.ValueList.get_value(fmt, y)
+                print("{} ".format(fmt), end="")
 
-                print("{} ".format(val), end="")
             print("}", end="")
-        else:
-            print("==")
+            # continue
 
-        width = structure.get_value("width")
-        height = structure.get_value("height")
+        # the python gobject introspection wrapper
+        # can pose problems in older version
+        # the type Gst.IntRange
+        # may not be available and thus cause a TypeError
+        # in such a case we query the string description
+        # of the Gst.Structure and extract the framerates
+        try:
+            if structure.to_string().find("[") != -1:
+                raise TypeError
 
-        print(" - {}x{} - ".format(width, height), end="")
+            width = structure.get_value("width")
+            height = structure.get_value("height")
+
+            print(" - {}x{} - ".format(width, height), end="")
+
+        except TypeError:
+
+            import re
+
+            # width handling
+
+            begin = structure.to_string().find("width=(int)[")
+            substr = structure.to_string()[begin:]
+            values = substr[substr.find("[")+1:substr.find("]")]
+            v = re.findall(r'\d+', values)
+
+            # assume first entry is smaller
+            width_min = v[0]
+            width_max = v[1]
+
+            # height handling
+
+            begin = structure.to_string().find("height=(int)[")
+            substr = structure.to_string()[begin:]
+            values = substr[substr.find("[")+1:substr.find("]")]
+            v = re.findall(r'\d+', values)
+
+            height_min = v[0]
+            height_max = v[1]
+
+            print(" - {}x{} <=> {}x{} - ".format(width_min, height_min, width_max, height_max), end="")
 
         # the python gobject introspection wrapper
         # can pose problems in older version
@@ -71,14 +124,30 @@ def print_formats(source):
         try:
             framerates = structure.get_value("framerate")
         except TypeError:
+
             import re
 
             substr = structure.to_string()[structure.to_string().find("framerate="):]
-            # try for frame rate lists
-            field, values, remain = re.split("{|}", substr, maxsplit=3)
-            rates = [x.strip() for x in values.split(",")]
-            for r in rates:
-                print("{} ".format(r), end="")
+
+            try:
+                # try for frame rate lists
+                field, values, remain = re.split("{|}", substr, maxsplit=3)
+                rates = [x.strip() for x in values.split(",")]
+                for r in rates:
+                    print("{} ".format(r), end="")
+            except ValueError:  # we have a GstFractionRange
+
+                values = substr[substr.find("[")+1:substr.find("]")]
+                v = re.findall(r'\d+', values)
+                fps_min_num = v[0]
+                fps_min_den = v[1]
+                fps_max_num = v[2]
+                fps_max_den = v[3]
+                # framerates are fractions thus one framerate euqals two values
+                print("{}/ {} <=> {}/{}".format(fps_min_num, fps_min_den,
+                                                fps_max_num, fps_max_den, end=""))
+
+            # printf line break
             print("")
             # we are done here
             continue
@@ -99,7 +168,7 @@ def print_formats(source):
 
         else:
             print("framerates not supported {}".format(type(framerates)))
-        # we are finished
+            # we are finished
         print("")
 
 
@@ -116,7 +185,7 @@ def main():
     # source = Gst.ElementFactory.make("tcambin")
 
     serial = None
-
+    serial = "35510248"
     if serial:
         source.set_property("serial", serial)
 
