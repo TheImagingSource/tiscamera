4097f27bec4b1877db099c3cb141035418a6d3a7
Edgar Thier <edgar.thier@theimagingsource.com>
2019-08-28 11:19:06 +0200

    gst: tcamsrc: Move debug message to trace

--- src/gstreamer-1.0/gsttcamsrc.cpp
+++ src/gstreamer-1.0/gsttcamsrc.cpp
@@ -1374,7 +1374,7 @@ static void gst_tcam_src_sh_callback (std::shared_ptr<tcam::ImageBuffer> buffer,
                                       void* data)
 {
     GstTcamSrc* self = GST_TCAM_SRC(data);
-    GST_INFO("sh callback");
+    GST_TRACE("sh callback");
 
 
     if (!self->is_running)
