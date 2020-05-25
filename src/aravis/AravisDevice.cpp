308b73e1f6502972542568718330ca8fd1b40f31
Edgar Thier <edgar.thier@theimagingsource.com>
2020-05-25 07:48:13 +0200

    aravis: Ask ArvBuffer for frmae id

--- src/aravis/AravisDevice.cpp
+++ src/aravis/AravisDevice.cpp
@@ -741,7 +741,7 @@ void AravisDevice::callback (ArvStream* stream __attribute__((unused)), void* us
 
             self->statistics.capture_time_ns = arv_buffer_get_system_timestamp(buffer);
             self->statistics.camera_time_ns = arv_buffer_get_timestamp(buffer);
-            self->statistics.frame_count++;
+            self->statistics.frame_count = arv_buffer_get_frame_id(buffer);
             self->statistics.is_damaged = false;
             // only way to retrieve actual image size
             size_t image_size = 0;
