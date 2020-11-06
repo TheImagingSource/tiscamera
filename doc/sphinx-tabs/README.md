dff627a2b040e74d0018ab2298796f642f664e96
Edgar Thier <edgar.thier@theimagingsource.com>
2020-11-06 10:02:27 +0100

    Revert "doc: Update sphinx-tabs"
    This reverts commit f7582c5a364a9c0d0b6259d71e8c0edb7f067e09.
    
    Commit prevents generating documentation on many systems.

--- doc/sphinx-tabs/README.md
+++ doc/sphinx-tabs/README.md
@@ -1,18 +1,16 @@
-# sphinx-tabs
-
-[![Github-CI][github-ci]][github-link]
-[![Coverage Status][codecov-badge]][codecov-link]
-[![PyPI][pypi-badge]][pypi-link]
+sphinx-tabs [![Build Status](https://travis-ci.org/djungelorm/sphinx-tabs.svg?branch=master)](https://travis-ci.org/djungelorm/sphinx-tabs)
+========================================
 
 Create tabbed content in [Sphinx documentation](http://www.sphinx-doc.org) when building HTML.
 
-For example, see the [Raw] code of [docs/index.rst](docs/index.rst) which generates the following:
+For example, see the [Raw] code of [test/index.rst](test/index.rst) which generates the following:
 
-A live demo can be found here: <https://sphinx-tabs.readthedocs.io>
+A live demo can be found here: https://djungelorm.github.io/sphinx-tabs/
 
 ![Tabs](/images/tabs.gif)
 
-## Installation
+Installation
+----------------------------------------
 
 ```bash
 pip install sphinx-tabs
@@ -24,34 +22,8 @@ To enable the extension in Sphinx, add the following to your conf.py:
 extensions = ['sphinx_tabs.tabs']
 ```
 
-If needed, there is a configuration option to allow additional builders to be considered compatible. For example, to add the `linkcheck` builder, add the following to your conf.py:
-
-```python
-sphinx_tabs_valid_builders = ['linkcheck']
-```
-
-If you are using [Read The Docs](https://readthedocs.org/) for building your documentation, the extension must be added as a requirement. Please add the following to `requirements.txt` at the root of the project:
-
-```
-sphinx-tabs
-```
-
-## Contributing
-
-We welcome all contributions!
-See the [EBP Contributing Guide](https://executablebooks.org/en/latest/contributing.html) for general details.
-
-The simplest way to run tests is to install [pre-commit](https://pre-commit.com/) for linting and [tox](https://tox.readthedocs.io) for unit tests and documentation build:
-
-```console
-$ pre-commit run --all
-```
-
-```console
-$ tox -p
-```
-
-## Basic Tabs
+Basic Tabs
+----------------------------------------
 
 Basic tabs can be coded as follows:
 
@@ -73,7 +45,8 @@ Basic tabs can be coded as follows:
 
 ![Tabs](/images/tabs.gif)
 
-## Grouped Tabs
+Grouped Tabs
+----------------------------------------
 
 Tabs can be grouped, so that changing the current tab in one area changes the current tab in the
 another area. For example:
@@ -110,7 +83,8 @@ another area. For example:
 
 ![Group Tabs](/images/groupTabs.gif)
 
-## Code Tabs
+Code Tabs
+----------------------------------------
 
 Tabs containing code areas with syntax highlighting can be created as follows:
 
@@ -153,10 +127,3 @@ Tabs containing code areas with syntax highlighting can be created as follows:
 ```
 
 ![Code Tabs](/images/codeTabs.gif)
-
-[github-ci]: https://github.com/executablebooks/sphinx-tabs/workflows/continuous-integration/badge.svg?branch=master
-[github-link]: https://github.com/executablebooks/sphinx-tabs
-[pypi-badge]: https://img.shields.io/pypi/v/sphinx-tabs.svg
-[pypi-link]: https://pypi.org/project/sphinx-tabs
-[codecov-badge]: https://codecov.io/gh/executablebooks/sphinx-tabs/branch/master/graph/badge.svg
-[codecov-link]: https://codecov.io/gh/executablebooks/sphinx-tabs
--- doc/sphinx-tabs/setup.py
+++ doc/sphinx-tabs/setup.py
@@ -1,60 +1,43 @@
-from os import path
-import re
 from setuptools import setup
 
-
-def get_version():
-    text = open(path.join(path.dirname(__file__), "sphinx_tabs", "__init__.py")).read()
-    match = re.compile(r"^__version__\s*\=\s*[\"\']([^\s\'\"]+)", re.M).search(text)
-    return match.group(1)
-
-
-with open("README.md") as readme:
-    long_description = readme.read()
-
 setup(
-    name="sphinx-tabs",
-    version=get_version(),
-    description="Tabbed views for Sphinx",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    author="djungelorm",
-    author_email="djungelorm@users.noreply.github.com",
-    packages=["sphinx_tabs"],
-    include_package_data=True,
-    url="https://github.com/executablebooks/sphinx-tabs",
-    license="MIT",
-    python_requires="~=3.6",
-    install_requires=["sphinx>=2,<4", "pygments"],
-    extras_require={
-        "testing": [
-            "coverage",
-            "pytest>=3.6,<4",
-            "pytest-cov",
-            "pytest-regressions",
-            "pygments",
-            "sphinx_testing",
-            "bs4",
+    name = 'sphinx-tabs',
+    version = '1.1.10',
+    author = 'djungelorm',
+    author_email = 'djungelorm@users.noreply.github.com',
+    packages = ['sphinx_tabs', 'sphinx_tabs.test'],
+    test_suite='sphinx_tabs.test',
+    package_data = {
+        'sphinx_tabs': [
+            'tabs.js',
+            'tabs.css',
+            'semantic-ui-2.2.10/*',
         ],
-        "code_style": ["pre-commit==2.6"],
     },
+    data_files = [("", ["LICENSE"])],
+    url = 'https://github.com/djungelorm/sphinx-tabs',
+    license = 'MIT',
+    description = 'Tab views for Sphinx',
+    install_requires = ['sphinx>=1.4'],
+    tests_require = ['sphinx>=1.4', 'docutils', 'pygments', 'sphinx_testing', 'lxml'],
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Plugins",
-        "Environment :: Web Environment",
-        "Framework :: Sphinx :: Extension",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Natural Language :: English",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python",
-        "Topic :: Documentation :: Sphinx",
-        "Topic :: Documentation",
-        "Topic :: Software Development :: Documentation",
-        "Topic :: Text Processing",
-        "Topic :: Utilities",
-    ],
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Plugins',
+        'Environment :: Web Environment',
+        'Framework :: Sphinx :: Extension',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python',
+        'Topic :: Documentation :: Sphinx',
+        'Topic :: Documentation',
+        'Topic :: Software Development :: Documentation',
+        'Topic :: Text Processing',
+        'Topic :: Utilities',
+    ]
 )
