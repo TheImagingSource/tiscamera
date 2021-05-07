55010b62b28b25faa4f1b487d9b8f8112f9ba45a
Edgar Thier <edgar.thier@theimagingsource.com>
2021-05-07 13:59:24 +0200

    v4l2: Add mappings

--- src/v4l2/v4l2_genicam_mapping.cpp
+++ src/v4l2/v4l2_genicam_mapping.cpp
@@ -13,6 +13,12 @@ static const std::map<int, struct tcam::v4l2::v4l2_genicam_mapping> v4l2_conv_di
             {},
         },
     },
+    { 0x00980902, // Saturation
+      { "", TCAM_PROPERTY_TYPE_DOUBLE, {} } },
+    { 0x00980903, // Hue
+      { "", TCAM_PROPERTY_TYPE_DOUBLE, {} } },
+    { 0x009a0901, // exposure_auto
+      { "ExposureAuto", TCAM_PROPERTY_TYPE_ENUMERATION, { { { 1, "Off" }, { 3, "On" } } } } },
     {
         0x009a0902, // exposure absolute
         {
@@ -21,42 +27,35 @@ static const std::map<int, struct tcam::v4l2::v4l2_genicam_mapping> v4l2_conv_di
             {},
         },
     },
+
     {
         0x009a0910, // privacy aka trigger
         { "TriggerMode", TCAM_PROPERTY_TYPE_ENUMERATION, { { { 0, "Off" }, { 1, "On" } } } },
     },
-    {
-        0x199e204,
-        {
-            "Gain",
-            TCAM_PROPERTY_TYPE_DOUBLE,
-            {},
-        }
-    } ,
-    { 0x199e205, // Gain Auto
-      {
-          "GainAuto",
-          TCAM_PROPERTY_TYPE_ENUMERATION,
-          { { { 0, "Off" }, { 1, "Continuous" } } },
-      } },
-    { 0x00980913, // Gain
-      { "", TCAM_PROPERTY_TYPE_DOUBLE, {} } },
-    { 0x199e202, // Auto Shutter
-      {
-          "ExposureAuto",
-          TCAM_PROPERTY_TYPE_ENUMERATION,
-          { { { 0, "Off" }, { 1, "Continuous" } } },
-      } },
     { 0x199e201, // Exposure Time (us)
       {
           "ExposureTime",
           TCAM_PROPERTY_TYPE_DOUBLE,
           {},
       } },
+    { 0x199e202, // Auto Shutter
+      {
+          "ExposureAuto",
+          TCAM_PROPERTY_TYPE_ENUMERATION,
+          { { { 0, "Off" }, { 1, "Continuous" } } },
+      } },
+    { 0x199e203, { "ExposureAutoReference", TCAM_PROPERTY_TYPE_UNKNOWN, {} } },
+    { 0x199e204, { "Gain", TCAM_PROPERTY_TYPE_DOUBLE, {}, } },
+    { 0x199e205, // Gain Auto
+      {
+          "GainAuto",
+          TCAM_PROPERTY_TYPE_ENUMERATION,
+          { { { 0, "Off" }, { 1, "Continuous" } } },
+      } },
     {
-        0x199e209,
+        0x199e207,
         {
-            "TriggerSoftware",
+            "BalanceWhitePreset",
             TCAM_PROPERTY_TYPE_UNKNOWN,
             {},
         },
@@ -65,15 +64,24 @@ static const std::map<int, struct tcam::v4l2::v4l2_genicam_mapping> v4l2_conv_di
         0x199e208,
         { "TriggerMode", TCAM_PROPERTY_TYPE_ENUMERATION, { { { 0, "Off" }, { 1, "On" } } } },
     },
-    { 0x0199e203, { "ExposureAutoReference", TCAM_PROPERTY_TYPE_UNKNOWN, {} } },
+    {
+        0x199e209,
+        {
+            "TriggerSoftware",
+            TCAM_PROPERTY_TYPE_UNKNOWN,
+            {},
+        },
+    },
+    { 0x00980913, // Gain
+      { "", TCAM_PROPERTY_TYPE_DOUBLE, {} } },
+
+
     { 0x00980910, // Gamma
       { "", TCAM_PROPERTY_TYPE_DOUBLE, {} } },
-    { 0x00980903, // Hue
-      { "", TCAM_PROPERTY_TYPE_DOUBLE, {} } },
-    { 0x00980902, // Saturation
-      { "", TCAM_PROPERTY_TYPE_DOUBLE, {} } },
-    { 0x0199e927, {"OffsetX", TCAM_PROPERTY_TYPE_UNKNOWN, {}}},
-    { 0x0199e928, {"OffsetY", TCAM_PROPERTY_TYPE_UNKNOWN, {}}},
+
+
+    { 0x0199e927, { "OffsetX", TCAM_PROPERTY_TYPE_UNKNOWN, {} } },
+    { 0x0199e928, { "OffsetY", TCAM_PROPERTY_TYPE_UNKNOWN, {} } },
     { 0x0199e210, { "TriggerDelay", TCAM_PROPERTY_TYPE_UNKNOWN, {} } },
     { 0x0199e211, { "StrobeEnable", TCAM_PROPERTY_TYPE_UNKNOWN, {} } },
     { 0x0199e212, { "StrobePolarity", TCAM_PROPERTY_TYPE_UNKNOWN, {} } },
@@ -82,58 +90,18 @@ static const std::map<int, struct tcam::v4l2::v4l2_genicam_mapping> v4l2_conv_di
     { 0x0199e215, { "StrobeDelay", TCAM_PROPERTY_TYPE_UNKNOWN, {} } },
     { 0x0199e217, { "GPIn", TCAM_PROPERTY_TYPE_INTEGER, {} } },
     { 0x0199e216, { "GPOut", TCAM_PROPERTY_TYPE_INTEGER, {} } },
-    {
-        0x0199e234,
-        { "TriggerPolarity", TCAM_PROPERTY_TYPE_UNKNOWN, {} },
-    },
-    {
-        0x0199e240,
-        { "TriggerNoiseSuppressionTime", TCAM_PROPERTY_TYPE_UNKNOWN, {} },
-    },
-    {
-        0x0199e242,
-        { "AutoFunctionsROILeft", TCAM_PROPERTY_TYPE_UNKNOWN, {} },
-    },
-    {
-        0x0199e243,
-        { "AutoFunctionsROITop", TCAM_PROPERTY_TYPE_UNKNOWN, {} },
-    },
-    {
-        0x0199e244,
-        { "AutoFunctionsROIWidth", TCAM_PROPERTY_TYPE_UNKNOWN, {} },
-    },
-    {
-        0x0199e245,
-        { "AutoFunctionsROIHeight", TCAM_PROPERTY_TYPE_UNKNOWN, {} },
-    },
-    {
-        0x0199e246,
-        { "BalanceWhiteMode", TCAM_PROPERTY_TYPE_UNKNOWN, {} },
-    },
-    {
-        0x0199e247,
-        { "BalanceWhiteAutoPreset", TCAM_PROPERTY_TYPE_UNKNOWN, {} },
-    },
-    {
-        0x0199e248,
-        { "BalanceWhiteComponentGreen", TCAM_PROPERTY_TYPE_UNKNOWN, {} },
-    },
-    {
-        0x0199e249,
-        { "BalanceWhiteTemperaturePreset", TCAM_PROPERTY_TYPE_UNKNOWN, {} },
-    },
-    {
-        0x0199e250,
-        { "BalanceWhiteTemperature", TCAM_PROPERTY_TYPE_UNKNOWN, {} },
-    },
-    {
-        0x0199e251,
-        {
-            "ReverseY",
-            TCAM_PROPERTY_TYPE_UNKNOWN,
-            {},
-        },
-    },
+    { 0x0199e234, { "TriggerPolarity", TCAM_PROPERTY_TYPE_UNKNOWN, {} }, },
+    { 0x0199e240, { "TriggerNoiseSuppressionTime", TCAM_PROPERTY_TYPE_UNKNOWN, {} }, },
+    { 0x0199e242, { "AutoFunctionsROILeft", TCAM_PROPERTY_TYPE_UNKNOWN, {} }, },
+    { 0x0199e243, { "AutoFunctionsROITop", TCAM_PROPERTY_TYPE_UNKNOWN, {} }, },
+    { 0x0199e244, { "AutoFunctionsROIWidth", TCAM_PROPERTY_TYPE_UNKNOWN, {} }, },
+    { 0x0199e245, { "AutoFunctionsROIHeight", TCAM_PROPERTY_TYPE_UNKNOWN, {} }, },
+    { 0x0199e246, { "BalanceWhiteMode", TCAM_PROPERTY_TYPE_UNKNOWN, {} },},
+    { 0x0199e247, { "BalanceWhiteAutoPreset", TCAM_PROPERTY_TYPE_UNKNOWN, {} }, },
+    { 0x0199e248, { "BalanceWhiteComponentGreen", TCAM_PROPERTY_TYPE_UNKNOWN, {} } },
+    { 0x0199e249, { "BalanceWhiteTemperaturePreset", TCAM_PROPERTY_TYPE_UNKNOWN, {} }, },
+    { 0x0199e250, { "BalanceWhiteTemperature", TCAM_PROPERTY_TYPE_UNKNOWN, {} }, },
+    { 0x0199e251, {"ReverseY", TCAM_PROPERTY_TYPE_UNKNOWN, {}, }, },
     {
         0x0199e252,
         {
@@ -142,22 +110,49 @@ static const std::map<int, struct tcam::v4l2::v4l2_genicam_mapping> v4l2_conv_di
             {},
         },
     },
-    {
-        0x0199e258,
-        { "AutoFunctionsROIPreset", TCAM_PROPERTY_TYPE_UNKNOWN, {} },
-    },
-    {
-        0x0199e256,
-        { "ExposureAutoUpperLimit", TCAM_PROPERTY_TYPE_DOUBLE, {} },
-    },
+    { 0x0199e258, { "AutoFunctionsROIPreset", TCAM_PROPERTY_TYPE_UNKNOWN, {} },},
+    { 0x0199e256, { "ExposureAutoUpperLimit", TCAM_PROPERTY_TYPE_DOUBLE, {} },},
     {
         0x0199e254,
         {
             "ExposureAutoUpperLimitAuto",
-            TCAM_PROPERTY_TYPE_UNKNOWN,
+            TCAM_PROPERTY_TYPE_ENUMERATION,
+            { { { 0, "Off" }, { 1, "On" } } },
+        },
+    },
+    {
+        0x0199e255,
+        {
+            "ExposureAutoLowerLimit",
+            TCAM_PROPERTY_TYPE_DOUBLE,
             {},
         },
     },
+    {
+        0x0199e256,
+        {
+            "ExposureAutoUpperLimit",
+            TCAM_PROPERTY_TYPE_DOUBLE,
+            {},
+        },
+    },
+    {
+        0x0199e259,
+        {
+            "GainAutoLowerLimit",
+            TCAM_PROPERTY_TYPE_DOUBLE,
+            {},
+        },
+    },
+    {
+        0x0199e260,
+        {
+            "GainAutoUpperLimit",
+            TCAM_PROPERTY_TYPE_DOUBLE,
+            {},
+        },
+    },
+
     {
         0x0199e261, // TriggerGlobalResetRelease
         {
