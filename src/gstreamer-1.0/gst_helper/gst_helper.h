bef87db35ffc21802327e61c2d1412ee9d2d08d1
Christopher Koschack <christopher.koschack@theimagingsource.com>
2021-07-06 09:57:37 +0200

    cmake/gst: Add gst_helper static library to project

---
+++ src/gstreamer-1.0/gst_helper/gst_helper.h
@@ -0,0 +1,112 @@
+
+#pragma once
+
+#include "gst_ptr.h"
+
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace gst_helper
+{
+std::string get_type_name(GstElement* element);
+std::string to_string(const GstCaps* caps);
+std::string to_string( const gst_ptr<GstCaps>& caps );
+
+gst_ptr<GstPad> get_static_pad( GstElement* elem, std::string name );
+gst_ptr<GstPad> get_static_pad( const gst_ptr<GstElement>& elem, std::string name );
+gst_ptr<GstPad> get_peer_pad( GstPad* pad ) noexcept;
+gst_ptr<GstPad> get_peer_pad( const gst_ptr<GstPad>& pad ) noexcept;
+gst_ptr<GstCaps> query_caps( GstPad* pad ) noexcept;
+gst_ptr<GstCaps> query_caps(const gst_ptr<GstPad>& pad) noexcept;
+bool caps_empty_or_any( const gst_ptr<GstCaps>& caps ) noexcept;
+
+std::vector<std::string> gst_string_list_to_vector( const GValue* gst_list );
+
+inline gst_ptr<GstPad> get_static_pad(GstElement* elem, std::string name)
+{
+    return gst_ptr<GstPad>::wrap(gst_element_get_static_pad(elem, name.c_str()));
+}
+inline gst_ptr<GstPad> get_static_pad(const gst_ptr<GstElement>& elem, std::string name)
+{
+    return get_static_pad(elem.get(), name);
+}
+
+inline gst_ptr<GstPad> get_peer_pad(GstPad* pad) noexcept
+{
+    return gst_ptr<GstPad>::wrap(::gst_pad_get_peer(pad));
+}
+
+inline gst_ptr<GstPad> get_peer_pad(const gst_ptr<GstPad>& pad) noexcept
+{
+    return get_peer_pad(pad.get());
+}
+
+inline gst_ptr<GstCaps> query_caps(GstPad* pad) noexcept
+{
+    return gst_ptr<GstCaps>::wrap( gst_pad_query_caps(pad, NULL) );
+}
+inline gst_ptr<GstCaps> query_caps(const gst_ptr<GstPad>& pad) noexcept
+{
+    return query_caps(pad.get());
+}
+
+
+inline std::string to_string(const GstCaps* caps)
+{
+    auto tmp = gst_caps_to_string(caps);
+    if (tmp == nullptr)
+    {
+        return {};
+    }
+
+    std::string rval = tmp;
+    g_free(tmp);
+    return rval;
+}
+inline std::string to_string(const gst_ptr<GstCaps>& caps)
+{
+    return to_string(caps.get());
+}
+
+
+inline bool caps_empty_or_any(const gst_ptr<GstCaps>& caps) noexcept
+{
+    return gst_caps_is_any(caps.get()) || gst_caps_is_empty(caps.get());
+}
+
+inline std::string get_type_name(GstElement* element)
+{
+    // this does not leak memory!!
+    const char* name =
+        g_type_name(gst_element_factory_get_element_type(gst_element_get_factory(element)));
+    return name;
+}
+
+inline std::vector<std::string> gst_string_list_to_vector(const GValue* gst_list)
+{
+    if (!GST_VALUE_HOLDS_LIST(gst_list))
+    {
+        GST_ERROR("Given GValue is not a list.");
+        return {};
+    }
+
+    unsigned int gst_list_size = gst_value_list_get_size(gst_list);
+
+    std::vector<std::string> ret;
+    ret.reserve(gst_list_size);
+    for (unsigned int i = 0; i < gst_list_size; ++i)
+    {
+        const GValue* val = gst_value_list_get_value(gst_list, i);
+        if (G_VALUE_TYPE(val) == G_TYPE_STRING)
+        {
+            ret.push_back(g_value_get_string(val));
+        }
+        else
+        {
+            GST_ERROR("List does not only contain strings.");
+        }
+    }
+    return ret;
+}
+} // namespace gst_helper
---
+++ src/gstreamer-1.0/gst_helper/gst_ptr.h
@@ -0,0 +1,224 @@
+#pragma once
+
+#include <gst/gst.h>
+
+
+namespace gst_helper
+{
+namespace detail
+{
+inline void unref_object(GstCaps* ptr) noexcept
+{
+    gst_caps_unref(ptr);
+}
+inline void unref_object(GstPad* ptr) noexcept
+{
+    gst_object_unref(ptr);
+}
+inline void unref_object(GstElement* ptr) noexcept
+{
+    gst_object_unref(ptr);
+}
+inline void unref_object(GstPlugin* ptr) noexcept
+{
+    gst_object_unref(ptr);
+}
+inline void unref_object(GstBufferPool* ptr) noexcept
+{
+    gst_object_unref(ptr);
+}
+//inline void    unref_object( GstStructure* ptr ) noexcept { gst_structure_free( ptr ); }
+
+inline void ref_object(GstCaps* ptr) noexcept
+{
+    gst_caps_ref(ptr);
+}
+inline void ref_object(GstPad* ptr) noexcept
+{
+    gst_object_ref(ptr);
+}
+inline void ref_object(GstElement* ptr) noexcept
+{
+    gst_object_ref(ptr);
+}
+inline void ref_object(GstPlugin* ptr) noexcept
+{
+    gst_object_ref(ptr);
+}
+inline void ref_object(GstBufferPool* ptr) noexcept
+{
+    gst_object_ref(ptr);
+}
+} // namespace detail
+
+template<class T> class gst_ptr
+{
+public:
+    gst_ptr() = default;
+
+    gst_ptr( nullptr_t ) noexcept {}
+
+    gst_ptr(const gst_ptr& op2) noexcept : ptr_ { ref(op2.ptr_) } {}
+    gst_ptr(gst_ptr&& op2) noexcept : ptr_ { std::exchange(op2.ptr_, nullptr) } {}
+    gst_ptr& operator=(const gst_ptr& op2) noexcept
+    {
+        unref(ptr_);
+        ptr_ = ref(op2.ptr_);
+        return *this;
+    }
+    gst_ptr& operator=(gst_ptr&& op2) noexcept
+    {
+        unref(ptr_);
+        ptr_ = std::exchange(op2.ptr_, nullptr);
+        return *this;
+    }
+
+    /**
+         * Wrap the passed in pointer. So do not increase the reference count.
+         */
+    static gst_ptr wrap(T* ptr) noexcept
+    {
+        return gst_ptr { ptr };
+    }
+    /**
+         * Consume the passed in reference. This means that if the pointer is floating, we remove this floating flag.
+         * The reference counter is not increased.
+         */
+    static gst_ptr consume(T* ptr) noexcept
+    {
+        if (ptr == nullptr)
+        {
+            return gst_ptr {};
+        }
+        if (g_object_is_floating(ptr))
+        {
+            g_object_ref_sink(ptr);
+        }
+        return gst_ptr { ptr };
+    }
+
+    static gst_ptr addref(T* ptr) noexcept
+    {
+        if (ptr == nullptr)
+        {
+            return gst_ptr {};
+        }
+        if (g_object_is_floating(ptr))
+        {
+            g_object_ref_sink(ptr);
+        }
+        else
+        {
+            ref( ptr );
+        }
+        return gst_ptr { ptr };
+    }
+
+    bool empty() const noexcept
+    {
+        return ptr_ == nullptr;
+    }
+    T* release() noexcept
+    {
+        return std::exchange(ptr_, nullptr);
+    }
+
+    T* get() const noexcept
+    {
+        return ptr_;
+    }
+
+    explicit operator bool() const noexcept
+    {
+        return !empty();
+    }
+
+    T& operator*() noexcept
+    {
+        return *ptr_;
+    }
+    const T& operator*() const noexcept
+    {
+        return *ptr_;
+    }
+
+    T* operator->() noexcept
+    {
+        return ptr_;
+    }
+    const T* operator->() const noexcept
+    {
+        return ptr_;
+    }
+
+    void reset() noexcept
+    {
+        unref(ptr_);
+    }
+    void reset(T* ptr)
+    {
+        unref(ptr_);
+        ptr_ = ref(ptr);
+    }
+
+
+private:
+    gst_ptr(T* ptr) noexcept : ptr_ { ptr } {}
+
+    T* ptr_ = nullptr;
+
+    static void unref(T* ptr) noexcept
+    {
+        using namespace detail;
+        unref_object(ptr);
+    }
+    static T* ref(T* ptr) noexcept
+    {
+        if (ptr == nullptr)
+            return nullptr;
+
+        using namespace detail;
+        ref_object(ptr);
+        return ptr;
+    }
+};
+
+template<class T> bool operator==(nullptr_t, const gst_ptr<T>& ptr) noexcept
+{
+    return ptr.empty();
+}
+template<class T> bool operator==(const gst_ptr<T>& ptr, nullptr_t ) noexcept
+{
+    return ptr.empty();
+}
+
+template<class T> bool operator!=(nullptr_t, const gst_ptr<T>& ptr) noexcept
+{
+    return !ptr.empty();
+}
+template<class T> bool operator!=(const gst_ptr<T>& ptr, nullptr_t) noexcept
+{
+    return !ptr.empty();
+}
+
+
+template<class T> gst_ptr<T> make_ptr(T* ptr) noexcept
+{
+    return gst_ptr<T>::consume(ptr);
+}
+
+
+template<class T> gst_ptr<T> make_addref_ptr(T* ptr) noexcept
+{
+    return gst_ptr<T>::addref(ptr);
+}
+
+template<class T> gst_ptr<T> make_consume_ptr(T* ptr) noexcept
+{
+    return gst_ptr<T>::consume(ptr);
+}
+template<class T> gst_ptr<T> make_wrap_ptr(T* ptr) noexcept
+{
+    return gst_ptr<T>::wrap(ptr);
+}
+} // namespace gst_helper
