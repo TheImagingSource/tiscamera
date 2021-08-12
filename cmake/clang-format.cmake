7bfcd61aa46423679a5776cfe01c8f7703a426b2
Christopher Koschack <christopher.koschack@theimagingsource.com>
2021-08-12 09:41:24 +0200

    cmake: Change clang-format directories to prevent <root>/libs from being formatted

--- cmake/clang-format.cmake
+++ cmake/clang-format.cmake
@@ -2,7 +2,7 @@
 include("functions")
 
 # get_all_cpp_files2(ALL_PROJECT_CPP_FILES)
-get_all_cpp_files(ALL_PROJECT_CPP_FILES "${CMAKE_CURRENT_SOURCE_DIR}/external;${CMAKE_CURRENT_BINARY_DIR};${CMAKE_CURRENT_SOURCE_DIR}/tools/firmware-update/33u/lib;${CMAKE_CURRENT_SOURCE_DIR}/src/lib;")
+get_all_cpp_files(ALL_PROJECT_CPP_FILES "${CMAKE_CURRENT_SOURCE_DIR}/external;${CMAKE_CURRENT_BINARY_DIR};${CMAKE_CURRENT_SOURCE_DIR}/tools/firmware-update/33u/lib;${CMAKE_CURRENT_SOURCE_DIR}/libs;")
 
 add_custom_target(clang-format
   COMMAND clang-format -i
