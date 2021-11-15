54db6246af2a6a40abd3b0d582baa2fa016384a8
Edgar Thier <edgar.thier@theimagingsource.com>
2021-11-15 13:16:56 +0100

    tcam-capture: Switch pipeline element order

--- tools/tcam-capture/config.h
+++ tools/tcam-capture/config.h
@@ -30,7 +30,7 @@ struct TcamCaptureConfig
     // if a capsfilter element named device-caps exists it will have the configured caps set
     // all tcam-property elements are named: tcam0, tcam1, etc
     // tcam0 is always source
-    QString pipeline = "tcambin name=tcam0 ! video/x-raw,format=BGRx ! videoconvert ! queue "
+    QString pipeline = "tcambin name=tcam0 ! video/x-raw,format=BGRx ! queue ! videoconvert "
         "! fpsdisplaysink video-sink=xvimagesink sync=false name=sink text-overlay=false signal-fps-measurements=true";
 
 
