d9ada738bfe4eb5fe18ad46ada7769e94f859815
Edgar Thier <edgar.thier@theimagingsource.com>
2022-10-19 11:01:52 +0200

    compilation: Fix clang compilation

--- libs/dutils_image/src/dutils_img_base/alignment_helper.h
+++ libs/dutils_image/src/dutils_img_base/alignment_helper.h
@@ -6,7 +6,7 @@
 
 namespace simd
 {
-    template<unsigned int alignment_value>
+    template<size_t alignment_value>
     inline unsigned int	calc_misalignment_bytecnt( void* p, unsigned int offset ) noexcept
     {
         return (reinterpret_cast<uintptr_t>(p) + offset) % alignment_value;
@@ -19,24 +19,25 @@ namespace simd
      * @param offset            The offset to add to ptr, for convenience.
      * @param alignment_value   The alignment value to reach. e.g. 64 to get the pointer to point to the start of a cache line
      */
-    template<unsigned int alignment_value>
-    inline uint8_t*	adjust_pointer_to_nxt_align( void* ptr, std::ptrdiff_t offset = 0 ) noexcept
+    template<size_t alignment_value>
+    inline void*	adjust_pointer_to_nxt_align( void* ptr, std::ptrdiff_t offset = 0 ) noexcept
     {
-        std::ptrdiff_t mod = ((reinterpret_cast<uintptr_t>(ptr) + offset) % alignment_value);
+        const auto address = reinterpret_cast<uintptr_t>(ptr);
+        std::ptrdiff_t mod = ((address + offset) % alignment_value);
         if( mod == 0 ) {
             return ptr;
         }
-        return ptr + alignment_value - mod;
+        return static_cast<void*>( static_cast<uint8_t*>(ptr) + alignment_value - mod );
     }
 
     template<size_t alignment_value>
     inline bool	    is_aligned_for_stream( void* p, int pitch ) noexcept
     {
-        const size_t address = static_cast<size_t>(static_cast<uint8_t*>(p) - static_cast<uint8_t*>(0));
+        const auto address = reinterpret_cast<uintptr_t>(p);
         if( address % alignment_value ) {
             return false;
         }
-        const size_t pitch_tmp = static_cast<size_t>(pitch < 0 ? -pitch : pitch);
+        const auto pitch_tmp = static_cast<size_t>(pitch < 0 ? -pitch : pitch);
         if( pitch_tmp % alignment_value ) {
             return false;
         }
@@ -46,7 +47,7 @@ namespace simd
     template<size_t alignment_value>
     inline bool	    is_aligned_for_stream( void* p ) noexcept
     {
-        size_t address = static_cast<uint8_t*>(p) - static_cast<uint8_t*>(0);
+        const auto address = reinterpret_cast<uintptr_t>(p);
         if( address % alignment_value ) {
             return false;
         }
